6691b1174b0f02d061ec1325a73d7a7e0b9c4a07
Nelson Bassey <nellybaz10@gmail.com>
2020-01-15 17:55:10 +0200

    update: change to mysql

--- bongalo_backend/settings.py
+++ bongalo_backend/settings.py
@@ -71,10 +71,22 @@ WSGI_APPLICATION = 'bongalo_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'bongalo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '8080'
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation

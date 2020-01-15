347834d8ee0ad1c4c082627bbcc0d50e908fd231
Nelson Bassey <nellybaz10@gmail.com>
2020-01-15 15:09:09 +0200

    feat: set admin to home route

--- bongalo_backend/urls.py
+++ bongalo_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('api/v1/apartment/', include('apartment.urls')),
     path('api/v1/auth/', include('authentication.urls')),
     path('api/v1/blog/', include('blog.urls')),

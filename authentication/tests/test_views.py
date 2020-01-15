a004c61b4c9a33b1cba110f61d607ebd1e9b0b45
Nelson Bassey <nellybaz10@gmail.com>
2020-01-15 12:48:57 +0200

    feat: write unit test for authentication app

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,11 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from authentication.models import User, UserProfile
+from rest_framework.test import APIRequestFactory, force_authenticate
+from authentication.views import LoginAPIView
 
 
 class TestViews(TestCase):
     def setup(self):
         self.client = Client()
+        self.factory = APIRequestFactory
 
     def test_register_user_view_success(self):
 
@@ -31,10 +34,19 @@ class TestViews(TestCase):
                                                                "first_name": "first_name", "last_name": "last_name"})
         self.assertEquals(response.status_code, 400)
 
-    # def test_login_view_success(self):
-    #
-    #     response = self.client.get(reverse("login_user", args=["bass106@gmail.comz14"]), secure=True)
-    #     self.assertEquals(response.status_code, 200)
+    def test_login_view_success(self):
+
+        user = User.objects.create(
+            username="test_user1x",
+            email="test_user1x@email.com",
+            password="pass"
+        )
+        # user = User.objects.get(username="")
+        view = LoginAPIView.as_view()
+        request = self.factory.get('login/test_user1x@email.com')
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEquals(response.status_code, 200)
 
     # def test_login_view_no_username(self):
     #

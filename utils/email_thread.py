e6e37b9d81b378a030d03360a3fc11ff0a866283
Nelson Bassey <nellybaz10@gmail.com>
2020-05-31 11:38:57 +0200

    Add email template

--- utils/email_thread.py
+++ utils/email_thread.py
@@ -2,6 +2,12 @@ import threading
 from django.core.mail import send_mail
 from django.conf import settings
 
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+
 
 def send_email(to, subject, message):
     send_mail(
@@ -11,6 +17,26 @@ def send_email(to, subject, message):
         [to],
         fail_silently=False,
     )
+
+
+def send_email_with_template():
+    message = Mail(
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to_emails='nellybaz10@gmail.com',
+        # html_content='<strong>and easy to do anywhere, even with Python</strong>'
+    )
+    message.dynamic_template_data = {
+        'name': 'Nelson',
+    }
+    message.template_id = '1c0076ca-2632-4457-b292-273cd87100ba'
+    try:
+        sendgrid_client = SendGridAPIClient(os.environ.get(settings.SENDGRID_API_KEY))
+        response = sendgrid_client.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
 
 
 class SendEmailThread(threading.Thread):
@@ -25,4 +51,5 @@ class SendEmailThread(threading.Thread):
         self.message = message
 
     def run(self):
-        send_email(self.recipient, self.subject, self.message)
+        # send_email(self.recipient, self.subject, self.message)
+        send_email_with_template()

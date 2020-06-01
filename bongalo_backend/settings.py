55f60d6679e1d5e784daee819b03cc15b5135296
Nelson Bassey <nellybaz10@gmail.com>
2020-06-01 11:38:22 +0200

    add new site to cors

--- bongalo_backend/settings.py
+++ bongalo_backend/settings.py
@@ -149,7 +149,17 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8080",
+    "http://localhost:8080",
+    "https://bongalo-frontend.herokuapp.com",
+    "https://bongalo.co",
+]
+
+CORS_ORIGIN_REGEX_WHITELIST = [
     "http://127.0.0.1:8080",
     "http://localhost:8080",
     "https://bongalo-frontend.herokuapp.com",

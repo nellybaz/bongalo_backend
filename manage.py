aef3c2bd3e798cf6da81fc6a5ced40aba46e3bcb
Nelson Bassey <nellybaz10@gmail.com>
2020-06-07 14:37:18 +0200

    uP

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import dotenv
 
 
 def main():
-    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bongalo_backend.settings')
     try:
         from django.core.management import execute_from_command_line

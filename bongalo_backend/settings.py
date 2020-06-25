4769f722aead11cf79aa1fcc506f7ef1736c36f1
Nelson Bassey <nellybaz10@gmail.com>
2020-06-25 17:11:21 +0200

    fix server error issues

--- bongalo_backend/settings.py
+++ bongalo_backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 
 REST_FRAMEWORK = {

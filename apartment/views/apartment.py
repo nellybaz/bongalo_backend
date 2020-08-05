b5479a996546e47ec2bf0201fba97522d5b4e9b3
Placide IRANDORA <placideirandora@gmail.com>
2020-08-05 14:38:53 +0200

    Improve the Linting with Flake8 for the Apartment View

--- apartment/views/apartment.py
+++ apartment/views/apartment.py
@@ -1,16 +1,17 @@
-from apartment.serializers import ApartmentSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
-from apartment.models import Apartment, Booking
 from rest_framework.permissions import IsAuthenticated
-from apartment.permissions import IsOwnerOrReadOnly as IsOwnerOnly
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status
+from datetime import datetime
 import datetime as dt
+
 from utils.email_thread import SendEmailThread, EmailService
 from authentication.serializers import UserProfileSerializer
-from datetime import  datetime
+from apartment.permissions import IsOwnerOrReadOnly as IsOwnerOnly
+from apartment.models import Apartment, Booking
+from apartment.serializers import ApartmentSerializer
 
 
 # TODO: Implement number of views for an apartment
@@ -18,40 +19,51 @@ from datetime import  datetime
 # TODO: Apartment ratings
 # TODO: Guest can see open bookings and can cancel
 # TODO: Create events and experience model
-# TODO:
-# TODO:
-# TODO:
 
 class ApartmentDetailsView(APIView):
     def get(self, request):
         apartment_uuid = self.request.query_params.get('uuid')
-        apartment_exists = Apartment.objects.filter(uuid=apartment_uuid, is_active=True)
+        apartment_exists = Apartment.objects.filter(uuid=apartment_uuid,
+                                                    is_active=True)
         if apartment_exists.exists():
-            apartment = Apartment.objects.get(uuid=apartment_uuid, is_active=True)
+            apartment = Apartment.objects.get(uuid=apartment_uuid,
+                                              is_active=True)
             owner_details_serialized = UserProfileSerializer(apartment.owner)
             serialized = ApartmentSerializer(
                 apartment)
             if serialized and owner_details_serialized:
-                all_bookings_for_apartment = Booking.objects.filter(apartment=apartment, is_completed=True, is_active=True)
+                all_bookings_for_apartment = Booking.objects.filter(
+                    apartment=apartment, is_completed=True, is_active=True)
                 unavailable_dates_for_apartment = []
                 today = datetime.now().date()
-                
+
                 if "full_place" in apartment.space:
                     for booking in all_bookings_for_apartment:
-                        if booking.date_from > today or today < booking.date_to:
-                            unavailable_dates_for_apartment.append({"from": booking.date_from, "to": booking.date_to})
+                        if (booking.date_from > today
+                           or today < booking.date_to):
+                            unavailable_dates_for_apartment.append
+                            ({"from": booking.date_from,
+                             "to": booking.date_to})
 
                 elif "private_room" in apartment.space:
-                    booking_for_getting_counts = []
+                    booking_counts = []
                     for booking in all_bookings_for_apartment:
-                        if booking.date_from > today or today < booking.date_to:
-                            booking_for_getting_counts.append({"from": booking.date_from, "to": booking.date_to})
-                    if len(booking_for_getting_counts) >= apartment.available_rooms:
-                        unavailable_dates_for_apartment = booking_for_getting_counts
-                        
+                        if (booking.date_from > today
+                           or today < booking.date_to):
+                            booking_counts.append(
+                                {"from": booking.date_from,
+                                 "to": booking.date_to})
+                    if (len(booking_counts)
+                            >= apartment.available_rooms):
+                        unavailable_dates_for_apartment = booking_counts
+
                 response_data = {
                     "responseCode": 1,
-                    "data": {**serialized.data, "owner_details": {**owner_details_serialized.data}, "unavailable_dates": unavailable_dates_for_apartment},
+                    "data": {**serialized.data,
+                             "owner_details":
+                             {**owner_details_serialized.data},
+                             "unavailable_dates":
+                             unavailable_dates_for_apartment},
                     "message": "ok"
                 }
 
@@ -62,7 +74,8 @@ class ApartmentDetailsView(APIView):
                 'message': 'Error occurred'
             }
 
-            return Response(data=response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data=response_data,
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         else:
             response_data = {
@@ -71,7 +84,8 @@ class ApartmentDetailsView(APIView):
                 "message": "apartment does not exists"
             }
 
-            return Response(data=response_data, status=status.HTTP_404_NOT_FOUND)
+            return Response(data=response_data,
+                            status=status.HTTP_404_NOT_FOUND)
 
 
 class ApartmentUpdateDeleteAPIView(APIView):
@@ -101,7 +115,8 @@ class ApartmentUpdateDeleteAPIView(APIView):
                 'message': 'Error occurred'
             }
 
-            return Response(data=response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data=response_data,
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         else:
             response_data = {
@@ -110,7 +125,8 @@ class ApartmentUpdateDeleteAPIView(APIView):
                 "message": "apartment does not exists"
             }
 
-            return Response(data=response_data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=response_data,
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         _apartment_exists = Apartment.objects.filter(uuid=request.data['uuid'])
@@ -129,7 +145,8 @@ class ApartmentUpdateDeleteAPIView(APIView):
                 "data": "apartment already deleted",
                 "message": "apartment already deleted",
             }
-            return Response(data=response_data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=response_data,
+                            status=status.HTTP_400_BAD_REQUEST)
 
         response_data = {
             "responseCode": 0,
@@ -160,7 +177,9 @@ class ApartmentCreateAPIView(APIView):
                     'lastName': request.user.last_name,
                     # 'verification_pin': verification_pin
                 }
-                email_thread = SendEmailThread(email_service.apartment_listing_confirmation, payload=payload)
+                email_thread = SendEmailThread(
+                    email_service.apartment_listing_confirmation,
+                    payload=payload)
                 email_thread.run()
             except BaseException as e:
                 print(str(e))
@@ -178,7 +197,8 @@ class ApartmentCreateAPIView(APIView):
             'message': 'Error occurred'
         }
 
-        return Response(data=response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=response_data,
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class ApartmentListAPIView(ListAPIView):
@@ -229,15 +249,18 @@ class ApartmentSearchAPIView(APIView):
         if check_in and check_out and apartment_type == "all":
             res = []
             all_apartment = Apartment.objects.filter(
-                is_available=True, max_guest_number__gte=number_of_guest, is_active=True)
+                is_available=True,
+                max_guest_number__gte=number_of_guest,
+                is_active=True)
             for apartment in all_apartment:
                 d_apartment = apartment
                 check_out_d = dt.datetime.strptime(
                     check_out + ' 00:00:00', '%d/%m/%Y %H:%M:%S')
                 check_in_d = dt.datetime.strptime(
                     check_in + ' 00:00:00', '%d/%m/%Y %H:%M:%S')
-                if check_out_d.date() < d_apartment.unavailable_from or check_in_d.date(
-                ) > d_apartment.unavailable_to:
+                if (check_out_d.date() < d_apartment.unavailable_from
+                   or check_in_d.date(
+                   ) > d_apartment.unavailable_to):
                     res.append(d_apartment.uuid)
 
             result = Apartment.objects.filter(uuid__in=res)
@@ -262,8 +285,9 @@ class ApartmentSearchAPIView(APIView):
                     check_out + ' 00:00:00', '%d/%m/%Y %H:%M:%S')
                 check_in_d = dt.datetime.strptime(
                     check_in + ' 00:00:00', '%d/%m/%Y %H:%M:%S')
-                if check_out_d.date() < d_apartment.unavailable_from or check_in_d.date(
-                ) > d_apartment.unavailable_to:
+                if (check_out_d.date() < d_apartment.unavailable_from
+                   or check_in_d.date(
+                   ) > d_apartment.unavailable_to):
                     res.append(d_apartment.uuid)
 
             result = Apartment.objects.filter(uuid__in=res)

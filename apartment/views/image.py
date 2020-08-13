1fddbaa63d60ef5072f8b5b36fac042f4b626ad1
Placide IRANDORA <placideirandora@gmail.com>
2020-08-13 11:33:10 +0200

    Improve the Linting with Flake8

--- apartment/views/image.py
+++ apartment/views/image.py
@@ -10,32 +10,40 @@ class ImageView(APIView):
         serialized = ImageSerializer(
             data=request.data, context={
                 "request": "post"})
+
         if serialized.is_valid():
             serialized.save()
-
             response_data = {"responseCode": 1, "data": serialized.data}
+
             return Response(data=response_data, status=status.HTTP_201_CREATED)
 
         response_data = {"responseCode": 0, "data": serialized.errors}
+
         return Response(data=response_data, status=status.HTTP_200_OK)
 
     def get(self, request):
         apartment = self.request.query_params.get("apartment")
-        if not Apartment.objects.filter(uuid=apartment).exists():
 
+        if not Apartment.objects.filter(uuid=apartment).exists():
             response_data = {
                 "responseCode": 0,
                 "data": "apartment does not exist"}
+
             return Response(data=response_data, status=status.HTTP_200_OK)
 
         apartment_obj = Images.objects.filter(apartment=apartment)
         serialized = ImageSerializer(
             apartment_obj, context={
                 "request": "post"}, many=True)
-        if serialized:
 
+        if serialized:
             response_data = {"responseCode": 1, "data": serialized.data}
+
             return Response(data=response_data, status=status.HTTP_200_OK)
 
-        response_data = {"responseCode": 0, "data": serialized.errors, 'message': 'Error occurred'}
-        return Response(data=response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        response_data = {"responseCode": 0,
+                         "data": serialized.errors,
+                         'message': 'Error occurred'}
+
+        return Response(data=response_data,
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
